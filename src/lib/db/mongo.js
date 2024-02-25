68a4ba371a05673044effac6a4396ff06bc3e4ed
JReis23 <cjfreis@gmail.com>
2024-02-25 16:16:43 +0100

    1st commit

--- src/lib/db/mongo.js
+++ src/lib/db/mongo.js
@@ -1,19 +1,6 @@
 import { MongoClient } from 'mongodb';
 import { MONGODB_URI } from '$env/static/private';
 
-// const mongoURI = MONGODB_URI;
-
-// export async function connectToMongoDB() {
-// 	const client = new MongoClient(mongoURI);
-// 	try {
-// 		console.log('Connecté à MongoDB avec succès :: ');
-// 		return await client.connect();
-// 	} catch (error) {
-// 		console.error('Erreur lors de la connexion à MongoDB via SSH : ', error);
-// 		throw error;
-// 	}
-// }
-
 export async function connectToMongoDB(db) {
 	const uri = MONGODB_URI;
 	const client = new MongoClient(uri);
